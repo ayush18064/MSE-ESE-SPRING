6ff4327010935235e45b38e53b94490a00eda70e
Ayush <118921090+ayush18064@users.noreply.github.com>
2025-05-19 22:41:29 +0530

    added frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import axios from "axios"
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  const [result, setResult] = useState([])
+  useEffect(() => {
+    const fetchResult = async () => {
+      try {
+
+        const response = await axios.get("/getAllStudents");
+        console.log(response.data);
+        setResult(response.data)
+      } catch (error) {
+        console.log(error);
+
+      }
+
+
+    }
+    fetchResult()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container mt-4">
+        <h3>Result</h3>
+        {
+          result.length > 0 ?
+            (
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>PRN</th>
+                    <th>Result</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {result.map((student) => (
+                    <tr key={student.id}>
+                      <td>{student.id}</td>
+                      <td>{student.name}</td>
+                      <td>{student.prn}</td>
+                      <td>{student.result.toFixed(2)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) :
+            <p>No data</p>
+        }
+      </div>
+    </>
   );
 }
 
